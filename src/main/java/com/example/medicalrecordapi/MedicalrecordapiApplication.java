03a102bfea67def3c919fa987277d1c416d2e332
gdrodrigues <gabrieldr@id.uff.br>
2020-05-04 21:07:44 -0300

    Projeto Spring

---
+++ src/main/java/com/example/medicalrecordapi/MedicalrecordapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicalrecordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalrecordapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalrecordapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medicalrecordapi/MedicalrecordapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicalrecordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalrecordapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
