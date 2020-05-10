0b8de115f6f2f198258237b46847af52aa0d0854
gdrodrigues <gabrieldr@id.uff.br>
2020-05-10 13:26:54 -0300

    Default  Swagger Configuration

---
+++ target/generated-sources/annotations/com/example/medicalrecordapi/mapper/DoctorMapperImpl.java
@@ -0,0 +1,51 @@
+package com.example.medicalrecordapi.mapper;
+
+import com.example.medicalrecordapi.dto.request.DoctorDTO;
+import com.example.medicalrecordapi.entity.Doctor;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-05-10T13:25:03-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.7 (AdoptOpenJDK)"
+)
+public class DoctorMapperImpl implements DoctorMapper {
+
+    @Override
+    public Doctor toModel(DoctorDTO doctorDTO) {
+        if ( doctorDTO == null ) {
+            return null;
+        }
+
+        Doctor doctor = new Doctor();
+
+        if ( doctorDTO.getBirthDate() != null ) {
+            doctor.setBirthDate( LocalDate.parse( doctorDTO.getBirthDate(), DateTimeFormatter.ofPattern( "dd-MM-yyyy" ) ) );
+        }
+        doctor.setId( doctorDTO.getId() );
+        doctor.setName( doctorDTO.getName() );
+        doctor.setCrm( doctorDTO.getCrm() );
+
+        return doctor;
+    }
+
+    @Override
+    public DoctorDTO toDTO(Doctor doctor) {
+        if ( doctor == null ) {
+            return null;
+        }
+
+        DoctorDTO doctorDTO = new DoctorDTO();
+
+        doctorDTO.setId( doctor.getId() );
+        doctorDTO.setName( doctor.getName() );
+        if ( doctor.getBirthDate() != null ) {
+            doctorDTO.setBirthDate( DateTimeFormatter.ISO_LOCAL_DATE.format( doctor.getBirthDate() ) );
+        }
+        doctorDTO.setCrm( doctor.getCrm() );
+
+        return doctorDTO;
+    }
+}
